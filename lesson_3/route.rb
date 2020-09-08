247acf81eaeb55e5175c2f51b86afeb5727189d5
Manziuchka <Manziuk.olga@gmail.com>
2020-09-08 23:02:10 +0300

    first commit

---
+++ lesson_3/route.rb
@@ -0,0 +1,21 @@
+class Route
+  attr_reader :stations
+
+  def initialize(first, last)
+    @first = first
+    @last = last
+    @stations = [first, last]
+  end
+
+  def add_station(station)
+    @stations.insert(-2, station)
+  end
+
+  def delete_station(station)
+    @stations.delete(station)
+  end
+
+  def show_stations
+    @stations
+  end
+end
---
+++ lesson_3/station.rb
@@ -0,0 +1,25 @@
+class Station 
+  attr_reader :name, :trains
+        
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def add_train(train)
+    @trains << train
+  end
+
+  def send_train(train)
+    @trains.delete(train)
+  end
+
+  def show_trains
+    @trains
+  end
+
+  def show_type_trains(type)
+    @trains.select {|train| train.type == type}
+  end
+
+end
